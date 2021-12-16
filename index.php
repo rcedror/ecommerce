2b6258ed9558329a7423b19eb21f3bb8e2b0de44
Renato Cedro <renatocedro0.1@gmail.com>
2021-12-15 23:43:19 -0300

    tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Rcedror\Page;
+use \Rcedror\PageAdmin;
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
   
 	$page = new Page();
+
+	$data = array("nome" => "Renato");
+	$page->setTpl("index",$data);
+});
+
+$app->get('/admin', function() {
+  
+	$page = new PageAdmin();
 
 	$data = array("nome" => "Renato");
 	$page->setTpl("index",$data);
